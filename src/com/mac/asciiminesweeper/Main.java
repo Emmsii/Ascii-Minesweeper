d6bb052fbb38852d71d7f4e30322a767703dc9cd
Emmsii <matty.93@hotmail.com>
2017-05-18 16:41:51 +0100

    Added Ascii Engine

--- src/com/mac/asciiminesweeper/Main.java
+++ src/com/mac/asciiminesweeper/Main.java
@@ -1,9 +1,43 @@
 package com.mac.asciiminesweeper;
 
+import com.mac.asciiminesweeper.ascii.AsciiPanel;
+import com.mac.asciiminesweeper.ascii.AsciiTerminal;
+import com.mac.asciiminesweeper.ascii.AsciiTerminalButton;
+
+import java.awt.*;
+import java.util.Random;
+
 /**
  * Project: Ascii Minesweeper
  * Created by Matt on 18/05/2017 at 04:20 PM.
  */
 public class Main {
     
+    public static final int WIDTH = 16;
+    public static final int HEIGHT = 16;
+    
+    private final AsciiTerminal terminal;
+    
+    public Main(String fontFile, int characterWidth, int characterHeight){
+        terminal = new AsciiTerminal("Title", new Dimension(WIDTH, HEIGHT), fontFile, characterWidth, characterHeight);
+
+        AsciiPanel panel = terminal.getAsciiPanel();
+
+        Random rand = new Random();
+
+        for(int i = 0; i < 16; i++) {
+            for(int j = 0; j < 10; j++) {
+                panel.write(i, j, (char)rand.nextInt(256), new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256)));
+            }
+        }
+
+        AsciiTerminalButton b1 = new AsciiTerminalButton(panel, "Click on me", 0, 12, Color.GREEN, Color.ORANGE);
+        panel.add(b1);
+        
+        terminal.repaint();
+    }
+    
+    public static void main(String[] args){
+        new Main("fonts/drake_10x10.png", 10, 10);
+    }
 }
---
+++ src/com/mac/asciiminesweeper/ascii/AsciiTerminalButton.java
@@ -0,0 +1,112 @@
+package com.mac.asciiminesweeper.ascii;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JComponent;
+
+/**
+ * A simple button
+ *
+ * @author julien MAITRE
+ *
+ */
+public class AsciiTerminalButton extends JComponent implements MouseListener {
+    
+    protected final AsciiPanel asciiPanel;
+    private String name;
+    private int x;
+    private int y;
+    protected boolean entered = false;
+    protected Color mouseCurrentColor;
+    protected Color mouseDefaultColor;
+    protected Color mouseEnteredColor;
+    protected Color mouseBackgroundColor;
+
+    public AsciiTerminalButton(AsciiPanel asciiPanel, String label, int x, int y, Color mouseDefaultColor, Color mouseEnteredColor) {
+        this.asciiPanel = asciiPanel;
+        this.name = label;
+        this.x = x;
+        this.y = y;
+        this.mouseCurrentColor = mouseDefaultColor;
+        this.mouseDefaultColor = mouseDefaultColor;
+        this.mouseEnteredColor = mouseEnteredColor;
+        setBounds(x*asciiPanel.getCharacterSize().width*asciiPanel.getScale(), y*asciiPanel.getCharacterSize().height*asciiPanel.getScale(), label.length()*asciiPanel.getCharacterSize().width*asciiPanel.getScale(), asciiPanel.getCharacterSize().height*asciiPanel.getScale());
+        this.addMouseListener(this);
+    }
+
+    public AsciiTerminalButton(AsciiPanel asciiPanel, String label, int x, int y, Color mouseDefaultColor, Color mouseEnteredColor, Color mouseBackgroundColor) {
+        this.asciiPanel = asciiPanel;
+        this.name = label;
+        this.x = x;
+        this.y = y;
+        this.mouseCurrentColor = mouseDefaultColor;
+        this.mouseDefaultColor = mouseDefaultColor;
+        this.mouseEnteredColor = mouseEnteredColor;
+        this.mouseBackgroundColor = mouseBackgroundColor;
+        setBounds(x*asciiPanel.getCharacterSize().width*asciiPanel.getScale(), y*asciiPanel.getCharacterSize().height*asciiPanel.getScale(), label.length()*asciiPanel.getCharacterSize().width*asciiPanel.getScale(), asciiPanel.getCharacterSize().height*asciiPanel.getScale());
+        this.addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // nothing
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // nothing
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // nothing
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        entered = true;
+        mouseCurrentColor = mouseEnteredColor;
+        asciiPanel.repaint(getBounds());
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        entered = false;
+        mouseCurrentColor = mouseDefaultColor;
+        asciiPanel.repaint(getBounds());
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        asciiPanel.writeString(x, y, name, mouseCurrentColor, mouseBackgroundColor != null ? mouseBackgroundColor : asciiPanel.getDefaultCharacterBackgroundColor());
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMouseDefaultColor(Color mouseDefaultColor) {
+        this.mouseDefaultColor = mouseDefaultColor;
+        if(!entered) {
+            mouseCurrentColor = mouseDefaultColor;
+            asciiPanel.repaint(getBounds());
+        }
+    }
+
+    public void setMouseEnteredColor(Color mouseEnteredColor) {
+        this.mouseEnteredColor = mouseEnteredColor;
+        if(entered) {
+            mouseCurrentColor = mouseEnteredColor;
+            asciiPanel.repaint(getBounds());
+        }
+    }
+}
